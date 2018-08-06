1dc37dd527e49eb99925b2654fffe65dcaedc250
DefiledSpec <defiledspec@gmail.com>
2018-08-06 13:43:51 -0500

    changed morgan to "short", added footer to survey.html

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(morgan('dev'))
+app.use(morgan('short'))
 
 app.use(express.static(path.join(__dirname, 'app/public/')))
 
