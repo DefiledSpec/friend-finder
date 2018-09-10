9b89b426781702c031212bbf54f0558117553d41
DefiledSpec <defiledspec@gmail.com>
2018-09-10 16:59:29 -0500

    updated sample data

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,33 +2,33 @@
 
 let friends = [
 	{
-		"name":"Ahmed",
-		"photo":"https://c-6rtwjumjzx7877x24nrlncx2ewfspjwx2ehtr.g00.ranker.com/g00/3_c-6bbb.wfspjw.htr_/c-6RTWJUMJZX77x24myyux78x3ax2fx2fnrlnc.wfspjw.htrx2fzx78jw_stij_nrlx2f05515x2f6556646821x2ftwnlnsfqx2fo-umtyt-z6x3fbx3d105x26vx3d05x26krx3doulx26knyx3dhwtux26hwtux3dkfhjx78x26n65h.rfwpx3dnrflj_$/$/$/$/$/$",
+		"name":"Tony",
+		"photo":"https://pbs.twimg.com/profile_images/681369932207013888/CHESpTzF_400x400.jpg",
 		"scores":[ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1 ]
 	},
-	{	"name":"Biason Trhfallgah",
-		"photo":"https://c-6rtwjumjzx7877x24nrlncx2ewfspjwx2ehtr.g00.ranker.com/g00/3_c-6bbb.wfspjw.htr_/c-6RTWJUMJZX77x24myyux78x3ax2fx2fnrlnc.wfspjw.htrx2fzx78jw_stij_nrlx2f05515x2f6556646803x2ftwnlnsfqx2fooo-umtyt-z6x3fbx3d105x26vx3d15x26krx3doulx26knyx3dhwtux26hwtux3dkfhjx78x26n65h.rfwpx3dnrflj_$/$/$/$/$/$",
+	{	"name":"Tiffany",
+		"photo":"https://laughingsquid.com/wp-content/uploads/2017/10/kate-mckinnon-hilariously-answers-questions-from-random-people-on-the-streets-of-new-yorkers2.png",
 		"scores":[ 4, 2, 5, 1, 3, 2, 2, 1, 3, 2 ]
 	},
-	{	"name":"Jeremy Scatman",
-		"photo":"https://c-6rtwjumjzx7877x24nrlncx2ewfspjwx2ehtr.g00.ranker.com/g00/3_c-6bbb.wfspjw.htr_/c-6RTWJUMJZX77x24myyux78x3ax2fx2fnrlnc.wfspjw.htrx2fzx78jw_stij_nrlx2f05513x2f6556892966x2ftwnlnsfqx2fmj-gjyyjw-bnuj-ymfy-x78rnqj-tkk-mnx78-kfhj-umtyt-z6x3fbx3d105x26vx3d05x26krx3doulx26knyx3dhwtux26hwtux3dkfhjx78x26n65h.rfwpx3dnrflj_$/$/$/$/$/$",
+	{	"name":"Jeremy",
+		"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfLyAIw7XB9bjD5HZGBrOE-f6z-d6ISbrK76SCG5ZahipIsc9w",
 		"scores":[ 5, 2, 5, 2, 4, 1, 3, 2, 5, 5 ]
 	},
-	{   "name":"Boberto T. Biene",
-		"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtYZ0_T0HVc7s74iaw3PxOVzUSrskZZ5X2lbZXkt4hyX_ZMJ6N",
+	{   "name":"Cynthia",
+		"photo":"http://images4.fanpop.com/image/photos/16300000/Random-people-random-16382026-600-800.jpg",
 		"scores":[ 3, 3, 4, 2, 2, 1, 3, 2, 2, 3 ]
 	},
-	{	"name":"Ritter Rider",
-		"photo":"https://c-6rtwjumjzx7877x24nrlncx2ewfspjwx2ehtr.g00.ranker.com/g00/3_c-6bbb.wfspjw.htr_/c-6RTWJUMJZX77x24myyux78x3ax2fx2fnrlnc.wfspjw.htrx2fzx78jw_stij_nrlx2f05515x2f6556646813x2ftwnlnsfqx2fo-umtyt-z6x3fbx3d105x26vx3d05x26krx3doulx26knyx3dhwtux26hwtux3dkfhjx78x26n65h.rfwpx3dnrflj_$/$/$/$/$/$",
+	{	"name":"Lisa",
+		"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0YPTWgXVmng_pOZlmgXwuJCyifYUWe_WNFD66GwraKkNIwuD7fQ",
 		"scores":[ 4, 3, 4, 1, 5, 2, 5, 3, 1, 4 ]
 	},
-	{	"name":"Jay Son",
-		"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBY137CiL1FKzYvRPjNwxZmY4pQr1Mr5iBTAJmP-8NWse-MIKx",
+	{	"name":"Jay",
+		"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJPCjulodZY_wL46KL4qM7HyTULNzAIjUG0yKN3sveSUQXun4Oyw",
 		"scores":[ 4, 4, 2, 3, 2, 2, 3, 2, 4, 5 ]
 	},
 	{	
-		"name":"Cynthia",
-		"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-ub_OOw6D0Ww8iy34U19Q5wwrqnABryze_ceN9c5CgUxo6xfZGQ",
+		"name":"Brent",
+		"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJF4OcAh5zKNNKrXdFHUzgdjSIeW6xbopgyuyvYMIKs2DhAU-2Gw",
 		"scores":[ 2, 4, 2, 2, 5, 2, 3, 5, 2, 5 ]
 	}
 ]
